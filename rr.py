5dc44fcb9eaaa45831b0487e5f70c85b7da3c553
Titoj740 <103675828+Titoj740@users.noreply.github.com>
2022-04-15 21:28:43 -0500

    Update rr.py

--- rr.py
+++ rr.py
@@ -1,3 +1,4 @@
 print("repito, solo son cambios tontos")
 print("ya ni si quiera se lo que estoy haciendo")
 print("que paso si agrego una cosa aquí")
+print("voy a editar la 4 línea en todas a ver que pasa")
